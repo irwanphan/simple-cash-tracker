40ee7bab5589dd888d3ec977df0607f4ee2ca429
irwanphan <irwanphan@gmail.com>
2024-11-13 15:35:24 +0700

    models: transaction

---
+++ lib/models/transaction.dart
@@ -0,0 +1,8 @@
+// lib/models/transaction.dart
+class Transaction {
+  final String title;
+  final int amount;
+  final String category;
+
+  Transaction(this.title, this.amount, this.category);
+}
